787e2e57f2067a7bbf5d420e0799b64b87295c27
sinka <xgao@zoyoe.com>
2023-12-23 09:24:28 +1100

    fix signature

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,11 +1,5 @@
 import React, { createRef, useEffect, useRef, useState } from "react";
 import { useAppSelector, useAppDispatch } from "../app/hooks";
-import {
-  loadStatus,
-  selectTasks,
-  tasksLoaded,
-  addProvingTask,
-} from "../data/statusSlice";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Button, Form } from "react-bootstrap";
@@ -23,24 +17,16 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import { State, ActionType } from "../types/game";
 import { ModalOptions } from "../types/layout";
 import {
-  selectL1Account,
+  //selectL1Account,
   selectL2Account,
 } from "../data/accountSlice";
 import BN from "bn.js";
-import { bytesToU64Hex } from "../utils/proof";
 import lotimage from "../images/lot.jpg";
-import { CurveField, PrivateKey, PublicKey } from "delphinus-curves/src/altjubjub";
-
-const initializeRustLib= async () => {
-  await initGameInstance();
-};
-
-const initialInstance = initializeRustLib();
-
+import { PrivateKey, PublicKey, bnToHexLe } from "delphinus-curves/src/altjubjub";
 
 export function Main() {
   const dispatch = useAppDispatch();
-  const [instance, setInstance] = useState<WasmInstance | null>(null);
+  const [wasmInstance, setWasmInstance] = useState<WasmInstance | null>(null);
   const [currentModal, setCurrentModal] = useState<ModalOptions | null>(null);
   const [lottoId, setLottoId] = useState<number>(0);
   const [lottoNumber, setLottoNumber] = useState<number>(0);
@@ -49,13 +35,13 @@ export function Main() {
   const [commitment, setCommitment] = useState<string>("0x1000");
   const [randomness, setRandomness] = useState<string>("");
   const [signature, setSignature] = useState<Array<string>>([]);
-  let account = useAppSelector(selectL1Account);
+  const [pubkey, setPubkey] = useState<Array<string>>([]);
   let l2account = useAppSelector(selectL2Account);
 
   useEffect(() => {
     initGameInstance().then((ins: WasmInstance) => {
       console.log("setting instance");
-      setInstance(ins);
+      setWasmInstance(ins);
     });
   }, []);
 
@@ -67,55 +53,39 @@ export function Main() {
     setLottoId(lottoId+1);
   }
 
-
-
   const commitRandomness = (hexnumber: string) => {
     setCommitment(hexnumber);
   }
 
-  const updateRandomness = () => {
-    let c = new BN(commitment.substring(2), 'hex');
-    let bytes = c.toArray("le",16);
-    const buf = new Uint8Array(bytes.length + 2);
-    buf.set(bytes, 0);
-    buf.fill(lottoId, bytes.length);
-    buf.fill(lottoNumber, bytes.length+1);
-    console.log(buf);
-    let valuebuf = buf;
-    let bns = bytesToU64Hex(valuebuf);
-    setSignature(bns);
-    let bns_str = bns.toString();
-    setRandomness(bns_str);
-  }
 
-  const handleCloseModal = () => {
-    setCurrentModal(null);
-  };
+  const msgToSign = () => {
+    const buf = new Uint8Array(2);
+    buf.fill(lottoId, 0);
+    buf.fill(lottoNumber, 1);
+    console.log(buf);
+    return buf;
+  }
 
   useEffect(() => {
     if (l2account) {
-        let c = new BN(commitment.substring(2), 'hex');
-        let bytes = c.toArray("le",16);
-        let prikey = PrivateKey.fromString(l2account);
-        console.log("update...");
-        const buf = new Uint8Array(bytes.length);
-        let pubkey = prikey.publicKey;
-        let pubkeyinputs = ["0x123"]
-                    //bytesToU64Hex(pubkey);
-        updateRandomness();
-        let sig_witness:Array<string> = signature.map((v) => v+":i64");
-        let pubkey_witness:Array<string> = pubkeyinputs.map((v) => v+":i64");
+        let msg = msgToSign();
+        console.log(l2account);
+        let prikey = PrivateKey.fromString(l2account.substring(2));
+        let sig = prikey.sign(msg);
+        let pkey = prikey.publicKey;
+        setPubkey([bnToHexLe(pkey.key.x.v), bnToHexLe(pkey.key.y.v)]);
+        setSignature([bnToHexLe(sig[0][0]), bnToHexLe(sig[0][1]), bnToHexLe(sig[1])]);
+        setRandomness(bnToHexLe(sig[1]));
+        let sig_witness:Array<string> = signature.map((v) => "0x" + v+ ":bytes-packed");
+        let pubkey_witness:Array<string> = pubkey.map((v) => "0x" + v+ ":bytes-packed");
         let witness = pubkey_witness;
         for (var s of sig_witness) {
             witness.push(s);
         }
         setWitness(witness);
-        setInstances(pubkeyinputs.map((v) => v+":i64"));
         commitRandomness(l2account);
     }
   }, [l2account, lottoId, lottoNumber]);
-
-  const offset = useRef(0); // Use ref instead of state to avoid unnecessary re-renders
 
   // Start or stop scrolling the background when the 'scroll' state changes
 
@@ -134,43 +104,41 @@ export function Main() {
         <>
           <Container style={{ position: "relative", top: "-10px", paddingBottom:"100px"}}>
             <Row>
-                    <Col>
-                    <img style={{width:"400px", display:"inline-block"}} src={lotimage}></img>
-                    </Col>
-
-            <Col>
-                    <Form>
-                            <InputGroup className="mb-3">
-                                    <InputGroup.Text id="basic-addon1">Current Lotto Id</InputGroup.Text>
-                                    <Form.Control
-                                            placeholder="Username"
-                                            aria-label="Username"
-                                            aria-describedby="basic-addon1"
-                                            value = {lottoId}
-                                    />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                    <InputGroup.Text id="basic-addon1">Your Lotto Number</InputGroup.Text>
-                                    <Form.Control
-                                            placeholder="Username"
-                                            aria-label="Username"
-                                            aria-describedby="basic-addon1"
-                                            value = {lottoNumber}
-                                    />
-                            </InputGroup>
-
-            <Button onClick={()=>pickLottoNumber(0)} active={lottoNumber == 0}> 0 </Button>
-            <Button onClick={()=>pickLottoNumber(1)} active={lottoNumber == 1}> 1 </Button>
-            <Button onClick={()=>pickLottoNumber(2)} active={lottoNumber == 2}> 2 </Button>
-            <Button onClick={()=>pickLottoNumber(3)} active={lottoNumber == 3}> 3 </Button>
-            <Button onClick={()=>pickLottoNumber(4)} active={lottoNumber == 4}> 4 </Button>
-            <Button onClick={()=>pickLottoNumber(5)} active={lottoNumber == 5}> 5 </Button>
-            <Button onClick={()=>pickLottoNumber(6)} active={lottoNumber == 6}> 6 </Button>
-            <Button onClick={()=>pickLottoNumber(7)} active={lottoNumber == 7}> 7 </Button>
-            <Button onClick={()=>pickLottoNumber(8)} active={lottoNumber == 8}> 8 </Button>
-            <Button onClick={()=>pickLottoNumber(9)} active={lottoNumber == 9}> 9 </Button>
-                    </Form>
-            </Col>
+              <Col>
+                <img style={{width:"400px", display:"inline-block"}} src={lotimage}></img>
+              </Col>
+              <Col>
+                 <Form>
+                   <InputGroup className="mb-3">
+                     <InputGroup.Text id="basic-addon1">Current Lotto Id</InputGroup.Text>
+                     <Form.Control
+                         placeholder="Username"
+                         aria-label="Username"
+                         aria-describedby="basic-addon1"
+                         value = {lottoId}
+                      />
+                   </InputGroup>
+                   <InputGroup className="mb-3">
+                      <InputGroup.Text id="basic-addon1">Your Lotto Number</InputGroup.Text>
+                      <Form.Control
+                          placeholder="Username"
+                          aria-label="Username"
+                          aria-describedby="basic-addon1"
+                          value = {lottoNumber}
+                      />
+                   </InputGroup>
+                   <Button onClick={()=>pickLottoNumber(0)} active={lottoNumber == 0}> 0 </Button>
+                   <Button onClick={()=>pickLottoNumber(1)} active={lottoNumber == 1}> 1 </Button>
+                   <Button onClick={()=>pickLottoNumber(2)} active={lottoNumber == 2}> 2 </Button>
+                   <Button onClick={()=>pickLottoNumber(3)} active={lottoNumber == 3}> 3 </Button>
+                   <Button onClick={()=>pickLottoNumber(4)} active={lottoNumber == 4}> 4 </Button>
+                   <Button onClick={()=>pickLottoNumber(5)} active={lottoNumber == 5}> 5 </Button>
+                   <Button onClick={()=>pickLottoNumber(6)} active={lottoNumber == 6}> 6 </Button>
+                   <Button onClick={()=>pickLottoNumber(7)} active={lottoNumber == 7}> 7 </Button>
+                   <Button onClick={()=>pickLottoNumber(8)} active={lottoNumber == 8}> 8 </Button>
+                   <Button onClick={()=>pickLottoNumber(9)} active={lottoNumber == 9}> 9 </Button>
+                </Form>
+              </Col>
             </Row>
             <Form>
                   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
@@ -186,8 +154,24 @@ export function Main() {
             </Form>
             <Form>
                   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Commands</Form.Label>
-                    <Form.Control as="textarea" rows={3} value ={witness}/>
+                    <Form.Label>PublicKey-X</Form.Label>
+                    <Form.Control as="input" value ={witness[0]}/>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>PublicKey-Y</Form.Label>
+                    <Form.Control as="input" value ={witness[1]}/>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Signature-X</Form.Label>
+                    <Form.Control as="input" value ={witness[2]}/>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Signature-Y</Form.Label>
+                    <Form.Control as="input" value ={witness[3]}/>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Signature-S</Form.Label>
+                    <Form.Control as="input" value ={witness[4]}/>
                   </Form.Group>
             </Form>
             <NewProveTask
